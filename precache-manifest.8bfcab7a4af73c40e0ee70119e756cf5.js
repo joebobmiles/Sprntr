136e89a1bf0f9e617bf425c7cf2ac286ed3bfb5f
Joseph R Miles <me@josephrmiles.com>
2020-05-23 14:57:45 -0700

    Updates

---
+++ precache-manifest.8bfcab7a4af73c40e0ee70119e756cf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "073ccfd021082c01409c578e5ac5e6e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69f88192ebbac2da234e",
+    "url": "/js/about.ed0d4b17.js"
+  },
+  {
+    "revision": "78418bd7c9806521708f",
+    "url": "/js/app.8c8d4b31.js"
+  },
+  {
+    "revision": "e01175763632e34c51d1",
+    "url": "/js/chunk-vendors.3eb65129.js"
+  },
+  {
+    "revision": "34ff91a29ecfc032a06937aab12c436f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
